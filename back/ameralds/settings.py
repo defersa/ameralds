2248f7c7be2ad679db4cd9f646ee7a3991f7755d
Makarov <a.makarov3@eapteka.ru>
2022-07-15 00:11:35 +0300

    v0.5.1.1 Fix viewer dialog.

--- back/ameralds/settings.py
+++ back/ameralds/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%c_e&7tk21uki)l0rdw8glo9$!tyh-#fb&d-7-1tb_heg5u5sm'
+SECRET_KEY = env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.ALLOWED_HOSTS
 
 # Application definition
 
--- front/src/app/shared/viewer/viewer-dialog/viewer-dialog.component.ts
+++ front/src/app/shared/viewer/viewer-dialog/viewer-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {Component, HostListener, Inject, OnInit, ViewEncapsulation} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import { ImageModelSmall } from 'src/app/interface/image.interface';
 
 @Component({
@@ -13,17 +13,29 @@ import { ImageModelSmall } from 'src/app/interface/image.interface';
 })
 export class AmstoreViewerDialogComponent implements OnInit {
 
-    constructor(@Inject(MAT_DIALOG_DATA) public data: {images: ImageModelSmall[], index: number}) { }
+    constructor(@Inject(MAT_DIALOG_DATA) public data: {images: ImageModelSmall[], index: number},
+                private _dialogRef: MatDialogRef<unknown>) { }
 
     ngOnInit(): void {
     }
 
-    public nextImage(): void {
+    public nextImage(event: MouseEvent): void {
+        event.stopPropagation();
         this.data.index = this.data.index === this.data.images.length - 1 ? 0 : this.data.index + 1;
     }
 
-    public prevImage(): void {
+    public prevImage(event: MouseEvent): void {
+        event.stopPropagation();
         this.data.index = this.data.index === 0 ? this.data.images.length - 1 : this.data.index - 1;
     }
 
+    public goToIndex(index: number, event: MouseEvent): void {
+        event.stopPropagation();
+        this.data.index = index;
+    }
+
+    @HostListener('click')
+    private _onClick(): void {
+        this._dialogRef.close();
+    }
 }
